38f6383e85b61531d4b4179a447ffcbdeb8a313a
Laurent Dibo-Cohen <laurent.dibocohen@levana.io>
2024-02-04 19:57:31 +0100

    add release package

---
+++ WeatherReport_Package/lwc/lDC_NewWeatherReportComponent/lDC_WeatherDataService.js
@@ -0,0 +1,16 @@
+export function getWeatherLabels(){
+    var labels = {
+            "humidity": "Humidity",
+            "temperature": "Temperature",
+            "weatherConditionLabel": "Conditions",
+            "windSpeed": "Wind Speed",
+            "weatherReport": "Weather Report",
+            "loading": "Loading...",
+            "kmh": "km/h",
+            "celsius": "°C",
+            "fahrenheit": "°F",
+            "mph": "mph",
+            "sendReport": "Send Report"
+        }
+        return labels;
+    }
